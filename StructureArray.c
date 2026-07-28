e8ed371504ee67b6194fb9de3f2abd1120ff43b5
Sanika-2212 <sanikamisal26@gmail.com>
2026-07-28 23:13:29 +0530

    Add files via upload

---
+++ StructureArray.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+//Declaration
+#pragma pack(1)
+struct Hello
+{
+    float f;     //4
+    int Arr[3];  //12
+}hobj;           //16
+int main()
+{
+    printf("Size of object is : %lu\n",sizeof(hobj));
+
+    return 0;
+}
