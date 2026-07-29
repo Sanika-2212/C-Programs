5576fc497db2845d2a318214c0bc849a934b7774
Sanika-2212 <sanikamisal26@gmail.com>
2026-07-29 23:54:15 +0530

    Add files via upload

---
+++ Write_file.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+
+int main()
+{
+    int fd = 0;
+    int Ret = 0;
+    char Arr[]= "Marvellous Infosystems";
+
+    fd=open("Marvellous.txt",O_RDWR);
+
+    if(fd==-1)
+    {
+        printf("Unable to open file\n");
+    }
+
+    else
+    {
+        printf("File gets open with fd : %d\n",fd);
+
+        Ret = write(fd,Arr,10);
+
+        printf("%d bytes gets written into the file\n",Ret);
+    }
+
+    close(fd);
+    
+    return 0;
+}
